245de606befc92fd65d6f874bf9a9dd7e9b7af83
minikowsky <marszalek.dominik@gmail.com>
2025-01-20 23:26:43 +0100

    Initial commit

---
+++ src/main/java/wsb/ordermanager/OrdermanagerApplication.java
@@ -0,0 +1,13 @@
+package wsb.ordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagerApplication.class, args);
+	}
+
+}
